8c733d29909a0bb563e40565982719398a5016df
ezemery <emereuwaonueze@gmail.com>
2024-08-14 21:50:17 +0100

    fix: next-auth cookie issues

--- apps/ai-headshots/src/app/api/auth/[...nextauth]/route.ts
+++ apps/ai-headshots/src/app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,26 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import { setCookie } from "../../../lib/cookie"
 import { config } from "../../../config";
 const handler = NextAuth({
-   providers: [
-        GoogleProvider({
-            clientId: config.GOOGLE_OAUTH_CLIENT_ID,
-            clientSecret: config.GOOGLE_OAUTH_CLIENT_KEY,
-             authorization: {
-                params: {
-                prompt: "consent",
-                access_type: "offline",
-                response_type: "code"
+            providers: [
+            GoogleProvider({
+                clientId: config.GOOGLE_OAUTH_CLIENT_ID,
+                clientSecret: config.GOOGLE_OAUTH_CLIENT_KEY,
+                authorization: {
+                    params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                    }
                 }
-            },
-             session: {
-                jwt: true,
-                maxAge: 15 * 24 * 60 * 60, // 30 days
-            },
-            jwt: {
-                secret: process.env.JWT_SECRET,
-            },
-            cookies: {
-                sessionToken: {
-                name: `__Secure-next-auth.session-token`,
-                options: {
-                    httpOnly: true,
-                    sameSite: 'lax',
-                    path: '/',
-                    secure: process.env.NODE_ENV === 'production',
-                },
-                },
-            },
-        }),
-        GitHubProvider({
-            clientId: config.GITHUB_OAUTH_CLIENT_ID,
-            clientSecret: config.GITHUB_OAUTH_CLIENT_KEY
-        })
-  ]
+            }),
+            GitHubProvider({
+                clientId: config.GITHUB_OAUTH_CLIENT_ID,
+                clientSecret: config.GITHUB_OAUTH_CLIENT_KEY
+            })
+        ],
 })
 
 export { handler as GET, handler as POST }
